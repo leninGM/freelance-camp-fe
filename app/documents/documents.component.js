17691a0975b0684faa3821ee8a3054138a9d2735
Lenin Godinez <lenin@michelada.io>
2018-05-16 20:03:03 -0500

    Add many things after too much days

--- app/documents/documents.component.js
+++ app/documents/documents.component.js
@@ -5,42 +5,38 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var Rx_1 = require("rxjs/Rx");
+var document_service_1 = require("./document.service");
 var DocumentsComponent = (function () {
-    function DocumentsComponent() {
+    function DocumentsComponent(documentService) {
+        this.documentService = documentService;
         this.pageTitle = "Document Dashboard";
-        this.documents = [
-            {
-                title: "My First Doc",
-                description: 'asdfasdfasdf asdfasd',
-                file_url: 'http://google.com',
-                updated_at: '11/11/16',
-                image_url: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a00dd14dd25d32b799b8e6e0270fd535&auto=format&fit=crop&w=1504&q=80',
-            },
-            {
-                title: "My Second Doc",
-                description: 'asdfasdfasdf asdfasd',
-                file_url: 'http://google.com',
-                updated_at: '11/11/16',
-                image_url: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a00dd14dd25d32b799b8e6e0270fd535&auto=format&fit=crop&w=1504&q=80',
-            },
-            {
-                title: "My Last Doc",
-                description: 'asdfasdfasdf asdfasd',
-                file_url: 'http://google.com',
-                updated_at: '11/11/16',
-                image_url: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a00dd14dd25d32b799b8e6e0270fd535&auto=format&fit=crop&w=1504&q=80',
-            }
-        ];
+        this.mode = "Observable";
     }
+    DocumentsComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        var timer = Rx_1.Observable.timer(0, 5000);
+        timer.subscribe(function () { return _this.getDocuments(); });
+    };
+    DocumentsComponent.prototype.getDocuments = function () {
+        var _this = this;
+        this.documentService.getDocuments()
+            .subscribe(function (documents) { return _this.documents = documents; }, function (error) { return _this.errorMessage = error; });
+    };
     DocumentsComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
             selector: 'documents',
             templateUrl: 'documents.component.html',
-            styleUrls: ['documents.component.css']
-        })
+            styleUrls: ['documents.component.css'],
+            providers: [document_service_1.DocumentService]
+        }),
+        __metadata("design:paramtypes", [document_service_1.DocumentService])
     ], DocumentsComponent);
     return DocumentsComponent;
 }());
--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -1,36 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Document } from './document';
+import { DocumentService } from './document.service';
 
 @Component({
 	moduleId: module.id,
 	selector: 'documents',
 	templateUrl: 'documents.component.html',
-	styleUrls: ['documents.component.css']
+	styleUrls: ['documents.component.css'],
+	providers: [ DocumentService ]
 })
-export class DocumentsComponent {
+export class DocumentsComponent implements OnInit {
 	pageTitle: string = "Document Dashboard"
+  documents: Document[];
+	errorMessage: string;
+	mode = "Observable";
 
-	documents: Document[] = [
-		{
-			title: "My First Doc",
-			description: 'asdfasdfasdf asdfasd',
-			file_url: 'http://google.com',
-			updated_at: '11/11/16',
-			image_url: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a00dd14dd25d32b799b8e6e0270fd535&auto=format&fit=crop&w=1504&q=80',
-		},
-		{
-			title: "My Second Doc",
-			description: 'asdfasdfasdf asdfasd',
-			file_url: 'http://google.com',
-			updated_at: '11/11/16',
-			image_url: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a00dd14dd25d32b799b8e6e0270fd535&auto=format&fit=crop&w=1504&q=80',
-		},
-		{
-			title: "My Last Doc",
-			description: 'asdfasdfasdf asdfasd',
-			file_url: 'http://google.com',
-			updated_at: '11/11/16',
-			image_url: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a00dd14dd25d32b799b8e6e0270fd535&auto=format&fit=crop&w=1504&q=80',
-		}
-	]
+	constructor(
+		private documentService: DocumentService
+	) {}
+
+	ngOnInit() {
+    let timer = Observable.timer(0, 5000);
+		timer.subscribe(() => this.getDocuments());
+	}
+
+	getDocuments() {
+		this.documentService.getDocuments()
+		    .subscribe(
+					documents => this.documents = documents,
+					error => this.errorMessage = <any>error
+				)
+	}
 }
